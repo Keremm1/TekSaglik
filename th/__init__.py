c2514c506097650ce42da6d52dc49a53ffedd071
Kerem Böyük <113975041+Keremm1@users.noreply.github.com>
2025-03-18 15:36:51 +0300

    Create __init__.py

---
+++ th/__init__.py
@@ -0,0 +1,7 @@
+from th.modules import (CNNModel,
+                        coatnet)
+
+from th.utils import (get_loader as loader,
+                      train_model as trainer,
+                      evaluate_model as evaluator,
+                      plot_metrics as plot)
